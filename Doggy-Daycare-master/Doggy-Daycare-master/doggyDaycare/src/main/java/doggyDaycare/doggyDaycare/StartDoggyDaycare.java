43505d2fb0e4adfd5790b3ec38761f228d7be9fd
EllissaP <35755014+EllissaP@users.noreply.github.com>
2018-04-11 11:02:26 -0500

    dog and user Dao

---
+++ Doggy-Daycare-master/Doggy-Daycare-master/doggyDaycare/src/main/java/doggyDaycare/doggyDaycare/StartDoggyDaycare.java
@@ -0,0 +1,15 @@
+package doggyDaycare.doggyDaycare;
+
+public class StartDoggyDaycare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Owner Tom = new Owner("Tom", "Smith", "991-5555", "19 Somewhere", "DSM", "IA", "12345");
+		System.out.println(Tom.toString());
+		
+		
+	}
+
+}
+
