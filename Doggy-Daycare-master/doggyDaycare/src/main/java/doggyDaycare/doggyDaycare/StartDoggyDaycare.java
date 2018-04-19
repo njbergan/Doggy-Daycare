269f0e1e1d5cc988a73aab5f5168b0d3148b0015
EllissaP <35755014+EllissaP@users.noreply.github.com>
2018-04-18 23:42:43 -0500

    started dog dao test

--- Doggy-Daycare-master/doggyDaycare/src/main/java/doggyDaycare/doggyDaycare/StartDoggyDaycare.java
+++ Doggy-Daycare-master/doggyDaycare/src/main/java/doggyDaycare/doggyDaycare/StartDoggyDaycare.java
@@ -1,15 +1,53 @@
 package doggyDaycare.doggyDaycare;
 
+import java.util.Date;
+import java.util.Scanner;
+
 public class StartDoggyDaycare {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		Owner Tom = new Owner("Tom", "Smith", "991-5555", "19 Somewhere", "DSM", "IA", "12345");
-		System.out.println(Tom.toString());
+		
+	static Scanner in = new Scanner(System.in);
+	static DogDao dogHelper = new DogDao();
 		
 		
+	static private void insertDog()
+	{
+		System.out.print("Enter owner ID: ");
+		String ownerId = in.nextLine();
+		System.out.print("Enter the dog's name: ");
+		String name = in.nextLine();
+		System.out.print("Enter the dog's breed: ");
+		String breed = in.nextLine();
+		System.out.print("Enter the dog's gender: ");
+		String gender = in.nextLine();
+		System.out.print("Enter the date: ");
+		String date = in.nextLine();
+		System.out.print("Does the dog have vaccinations?(Y or N): ");
+		String vaccinations = in.nextLine();
+		System.out.print("Enter the last visit: ");
+		String lastVisit = in.nextLine();
+		System.out.print("Is the dog active?(Y or N): ");
+		String activeFlag = in.nextLine();
+		
+		boolean bVaccinations = false;
+		if (vaccinations.equals("Y"))
+		{
+			bVaccinations = true; 
+		}
+		
+		boolean bActiveFlag = false;
+		if (activeFlag.equals("Y"))
+		{
+			bActiveFlag = true;
+		}
+		
+		
+		//not sure how to add dates here to test
+		Dog toAdd = new Dog(Integer.parseInt(ownerId),name, breed, gender, null, bVaccinations, null, bActiveFlag);
+		dogHelper.insertDog(toAdd);
 	}
 
-}
+	}
+
+
 
